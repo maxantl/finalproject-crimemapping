ee6c6d5f1ce9ed195b9592266e984e587a288f39
Max <2202788@slu.edu.ph>
2022-07-16 15:07:11 +0800

    add folium map

--- DATAMINING/WEBAPP/views.py
+++ DATAMINING/WEBAPP/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+import folium
+from folium import plugins
+
 def test(request):
-    return render(request,'WEBAPP/test.html')
+    map1 = folium.Map([12, 122], tiles='CartoDB Dark_Matter', zoom_start=6, max_zoom=8)._repr_html_()
+    context = {
+        'map1': map1,
+    }
+    return render(request,'WEBAPP/test.html',context)
