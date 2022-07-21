b53f2a9af91246210f3c74bc5af8c3d97f4519c5
Max <2202788@slu.edu.ph>
2022-07-21 13:14:58 +0800

    edit base.html edit test.html

--- DATAMINING/WEBAPP/views.py
+++ DATAMINING/WEBAPP/views.py
@@ -3,8 +3,8 @@ import folium
 from folium import plugins
 
 def test(request):
-    map1 = folium.Map(width=900, height= 540,location=[12, 122], tiles='CartoDB Dark_Matter', zoom_start=6, max_zoom=8)._repr_html_()
-    map2 = folium.Map(width=900, height= 540,location=[12, 122], tiles='CartoDB Dark_Matter', zoom_start=6, max_zoom=8)._repr_html_()
+    map1 = folium.Map(location=[12, 122], tiles='CartoDB Dark_Matter', zoom_start=6, max_zoom=8)._repr_html_()
+    map2 = folium.Map(location=[12, 122], tiles='CartoDB Dark_Matter', zoom_start=6, max_zoom=8)._repr_html_()
     context = {
         'map1': map1,
         'map2': map2,
